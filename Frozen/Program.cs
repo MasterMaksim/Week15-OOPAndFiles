0beaae79aec9ed6e7d0e9fc8cd1000cf18e7841a
Maksim Muravjov <mmuravjo@tktk.ee>
2021-12-27 19:20:42 +0200

    Add New OOP

---
+++ Frozen/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Frozen
+{
+    class Program
+    {
+        class SecretSanta
+        {
+            string name;
+            string gift;
+
+            public SecretSanta(string _name, string _gift)
+            {
+                name = _name;
+                gift = _gift;
+            }
+
+            //getters for SecretSanta
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Gift
+            {
+                get { return gift; }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<SecretSanta> mySecretSanta = new List<SecretSanta>();
+            string[] wishesFromFile = GetDataFromFile();
+
+            foreach (string line in wishesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                SecretSanta newWish = new SecretSanta(tempArray[0], tempArray[1]);
+                mySecretSanta.Add(newWish);
+            }
+
+            foreach (SecretSanta wishFromList in mySecretSanta)
+            {
+                Console.WriteLine($"{wishFromList.Name} wants {wishFromList.Gift}");
+            }
+        }
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach (string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\admin\samples\frozen.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+    }
+}
